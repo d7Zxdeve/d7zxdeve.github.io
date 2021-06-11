9d594b455c2dcb92d3ceb5cc0bd4dc6f234653ba
Naresh Ram Prasad Yella <46834913+d7Zxdeve@users.noreply.github.com>
2021-06-11 11:58:06 -0400

    Add files via upload

--- es2015-polyfills.js
+++ es2015-polyfills.js
@@ -7022,8 +7022,8 @@ for (var collections = getKeys(DOMIterables), i = 0; i < collections.length; i++
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/ramyella/WebstormProjects/side-project/myapp/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js");
-module.exports = __webpack_require__(/*! /Users/ramyella/WebstormProjects/side-project/myapp/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js");
+__webpack_require__(/*! /Users/ramyella/Downloads/Portfolio/myapp/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js");
+module.exports = __webpack_require__(/*! /Users/ramyella/Downloads/Portfolio/myapp/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js");
 
 
 /***/ })
--- styles.js
+++ styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/ramyella/WebstormProjects/side-project/myapp/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /Users/ramyella/Downloads/Portfolio/myapp/src/styles.css */"./src/styles.css");
 
 
 /***/ })
